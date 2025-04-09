63d40a9f8d20ffdd3afb41a8a35e0b0e2c6f3bf3
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2025-04-09 12:29:06 +0300

    init fix

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,3 @@
-from app.models.user import User
-
+from app.models.user import User, Student, Teacher, Lesson, Group, Subscription, SubscriptionTemplate, Payment, Slot
+from app.models.association import TeacherLesson, TeacherGroup, StudentGroup, LessonSubscription, SubscriptionLessonType
+from app.models.base import BaseModel 
