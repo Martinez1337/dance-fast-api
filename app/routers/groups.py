40a679c1a4e2bed51cc1feef10e9ce9f6c00d3b0
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2025-04-12 12:25:30 +0300

    added groups router

--- app/routers/groups.py
+++ app/routers/groups.py
@@ -21,63 +21,58 @@ async def create_group(
         group_data: schemas.GroupBase,
         db: Session = Depends(get_db)
 ):
-    user = db.query(models.User).filter(models.User.id == student_data.user_id).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Пользователя с идентификатором {student_data.user_id} не существует",
-        )
-
-    level = db.query(models.Level).filter(models.Level.id == student_data.level_id).first()
+    level = db.query(models.Level).filter(models.Level.id == group_data.level_id).first()
     if not level:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Уровня подготовки с идентификатором {student_data.level_id} не существует",
+            detail=f"Уровня подготовки с идентификатором {group_data.level_id} не существует",
         )
 
-    student = models.Student(
-        user_id=student_data.user_id,
-        level_id=student_data.level_id,
+    group = models.Group(
+        name=group_data.name,
+        description=group_data.description,
+        max_capacity=group_data.max_capacity,
+        level_id=group_data.level_id,
         created_at=datetime.now(timezone.utc)
     )
 
-    db.add(student)
+    db.add(group)
     db.commit()
-    db.refresh(student)
+    db.refresh(group)
 
-    return student
-
-
-@router.get("/", response_model=List[schemas.StudentBaseInfo])
-async def get_all_students(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    students = db.query(models.Student).offset(skip).limit(limit).all()
-    return students
+    return group
 
 
-@router.get("/full-info", response_model=List[schemas.StudentFullInfo])
-async def get_all_students_full_info(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    students = db.query(models.Student).offset(skip).limit(limit).all()
-    return students
+@router.get("/", response_model=List[schemas.GroupBaseInfo])
+async def get_all_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    groups = db.query(models.Group).offset(skip).limit(limit).all()
+    return groups
 
 
-@router.get("/{student_id}", response_model=schemas.StudentBaseInfo)
-async def get_student_by_id(student_id: uuid.UUID, db: Session = Depends(get_db)):
-    student = db.query(models.Student).filter(models.Student.id == student_id).first()
-    if not student:
+@router.get("/full-info", response_model=List[schemas.GroupFullInfo])
+async def get_all_groups_full_info(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    groups = db.query(models.Group).offset(skip).limit(limit).all()
+    return groups
+
+
+@router.get("/{group_id}", response_model=schemas.GroupBaseInfo)
+async def get_group_by_id(group_id: uuid.UUID, db: Session = Depends(get_db)):
+    group = db.query(models.Group).filter(models.Group.id == group_id).first()
+    if not group:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Студент не найден"
+            detail="Группа не найдена"
         )
-    return student
+    return group
 
 
-@router.get("/full-info/{student_id}", response_model=schemas.StudentFullInfo)
-async def get_student_full_info_by_id(student_id: uuid.UUID, db: Session = Depends(get_db)):
-    student = db.query(models.Student).filter(models.Student.id == student_id).first()
-    if not student:
+@router.get("/full-info/{group_id}", response_model=schemas.GroupFullInfo)
+async def get_group_full_info_by_id(group_id: uuid.UUID, db: Session = Depends(get_db)):
+    group = db.query(models.Group).filter(models.Group.id == group_id).first()
+    if not group:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Студент не найден"
+            detail="Группа не найдена"
         )
 
-    return student
+    return group
